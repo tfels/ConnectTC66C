0c32214ea751a1e3564b4fd5269416f9c9ad2109
Tim Felser <tim@felser-net.de>
2021-12-29 13:41:15 +0100

    initial check-in: android studio default files

---
+++ app/src/main/java/de/felser_net/connecttc66c/MainActivity.kt
@@ -0,0 +1,11 @@
+package de.felser_net.connecttc66c
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
