1d6bded55d6440a5a8bbdf55f94cf265612dfd9b
tfels <32812295+tfels@users.noreply.github.com>
2022-01-01 18:37:50 +0100

    add filter switch
    and reorganize ble scan start/stop: move into functions

--- app/src/main/java/de/felser_net/connecttc66c/BleScanFragment.kt
+++ app/src/main/java/de/felser_net/connecttc66c/BleScanFragment.kt
@@ -2,11 +2,10 @@ package de.felser_net.connecttc66c
 
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
-import android.bluetooth.le.BluetoothLeScanner
-import android.bluetooth.le.ScanCallback
-import android.bluetooth.le.ScanResult
+import android.bluetooth.le.*
 import android.content.Context
 import android.os.Bundle
+import android.os.ParcelUuid
 import android.os.SystemClock
 import android.util.Log
 import android.view.View
@@ -18,6 +17,7 @@ class BleScanFragment : ListFragment() {
     private var mBtAdapter:BluetoothAdapter? = null
     private var mBleScanner: BluetoothLeScanner? = null
     private var mBleDeviceListAdapter: BleDeviceListAdapter? = null
+    private var mApplyScanFilter= false
 
     private val mBleScanCallback: ScanCallback = object : ScanCallback() {
         override fun onScanResult(callbackType: Int, result: ScanResult?) {
@@ -25,9 +25,11 @@ class BleScanFragment : ListFragment() {
             val lastSeenSec = result?.let { (SystemClock.elapsedRealtimeNanos() - it.timestampNanos).toFloat() / 1000 / 1000 / 1000}
             Log.i("BleScanFragment", "onScanResult():\n" +
                     "name:     ${result?.device?.name}\n" +
+                    "scanName: ${result?.scanRecord?.deviceName}\n" +
                     "address:  ${result?.device?.address}\n" +
                     "rssi:     ${result?.rssi} dBm\n" +
-                    "lastSeen: ${lastSeenSec}s ago"
+                    "lastSeen: ${lastSeenSec}s ago\n" +
+                    "serviceUuids: ${result?.scanRecord?.serviceUuids}\n"
             )
 
             if(result != null) {
@@ -46,23 +48,42 @@ class BleScanFragment : ListFragment() {
 
     override fun onResume() {
         super.onResume()
-
-        val bluetoothManager = requireContext().getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
-        mBtAdapter = bluetoothManager.adapter
-        mBleScanner = mBtAdapter?.bluetoothLeScanner
-        Log.i("BleScanFragment", "startScan")
-        mBleScanner!!.startScan(mBleScanCallback)
+        startBleScan()
     }
 
     override fun onPause() {
-        mBleScanner!!.stopScan(mBleScanCallback)
-        mBleDeviceListAdapter?.clear()
+        stopBleScan()
         super.onPause()
     }
 
     override fun onDestroyView() {
-        Log.i("BleScanFragment", "stopScan")
-        mBleScanner!!.stopScan(mBleScanCallback)
+        stopBleScan()
         super.onDestroyView()
     }
+
+    private fun startBleScan() {
+        if(mBleScanner != null) {
+            stopBleScan()
+        } else {
+            val bluetoothManager = requireContext().getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+            mBtAdapter = bluetoothManager.adapter
+            mBleScanner = mBtAdapter?.bluetoothLeScanner
+        }
+
+        Log.i("BleScanFragment", "startScan")
+        if(mApplyScanFilter) {
+            val scanFilter = listOf(ScanFilter.Builder().setServiceUuid(ParcelUuid.fromString("0000ffe0-0000-1000-8000-00805f9b34fb")).build())
+            val scanSettings = ScanSettings.Builder().setScanMode(ScanSettings.SCAN_MODE_LOW_POWER).build()
+            mBleScanner?.startScan(scanFilter, scanSettings, mBleScanCallback)
+        } else {
+            mBleScanner?.startScan(mBleScanCallback)
+        }
+    }
+
+    private fun stopBleScan() {
+        Log.i("BleScanFragment", "stopScan")
+        mBleScanner?.stopScan(mBleScanCallback)
+        mBleDeviceListAdapter?.clear()
+    }
+
 }
